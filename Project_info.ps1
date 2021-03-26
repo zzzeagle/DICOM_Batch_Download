a7bd1f23a0fba919d6ff1e4fa7f15e742ad399e9
Zachary Eagle <zacharyeagle@gmail.com>
2021-03-26 10:24:44 -0700

    Revamp for DCMTK 3.6 and PowerShell 7
    Now runs on Linux. Improved logging

--- Project_info.ps1
+++
@@ -1,6 +0,0 @@
-﻿#Enter a unique project name
-$ProjectName = ""
-#Enter the path to an CSV file that accession, series #, image #
-$AccList = ""
-#Working directory for your PACS downloads
-$ProjectFolder = ""
